7693c7ab4c3166ef1f85eadc1d93b8b9cc7aaa83
khiari-mohamed <mouhamed.khiari.93093292@gmail.com>
2025-08-21 20:25:55 +0100

    push

--- src/components/StaticPages/PolitiquedesCookies.tsx
+++ src/components/StaticPages/PolitiquedesCookies.tsx
@@ -9,7 +9,7 @@ const PolitiquedesCookies = ({ page }: Props) => {
   return (
     <>
       <Header />
-      <div className="w-full bg-[#F2F2F2] flex flex-col items-center pt-0 pb-12 min-h-screen overflow-x-hidden">
+      <div className="w-full bg-[#F2F2F2] flex flex-col items-center pt-0 xl:pt-14 pb-12 min-h-screen overflow-x-hidden">
         <div className="w-full max-w-9xl bg-white shadow-md py-8 px-4 sm:px-10 mt-8 z-10 relative mx-auto">
           <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-center text-[#FF5000]">
             {page.title}
